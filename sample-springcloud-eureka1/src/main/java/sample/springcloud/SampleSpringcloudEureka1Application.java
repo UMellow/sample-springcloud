5bc0cbb46d6c6cbd9dcad8d071548caf31fc36fc
Doctor <zhaoyi@olasharing.com>
2019-02-19 17:39:18 +0800

    initial

---
+++ sample-springcloud-eureka1/src/main/java/sample/springcloud/SampleSpringcloudEureka1Application.java
@@ -0,0 +1,13 @@
+package sample.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSpringcloudEureka1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleSpringcloudEureka1Application.class, args);
+	}
+
+}
